7d880f209a4e219397a1eb167663d166e6c44905
nymbs <karen.vardanyan2009@gmail.com>
2026-04-17 20:18:47 +0400

    string-2315

---
+++ string-2315.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s = "l|*e*et|c**o|*de|";
+    int count1 = 0; 
+    int count = 0;
+
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == '|'){
+            count1++;
+        }
+        else if(s[i] == '*' && count1 % 2 == 0){
+            count++;
+        }
+    }
+
+    return count;
+}
