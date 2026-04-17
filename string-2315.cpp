2bdb710978b7d68b5d49b97bd8324a49306c6836
GXAfam <khachatryan651@gmail.com>
2026-04-17 20:18:56 +0400

    string-2315

---
+++ string-2315.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    int flag = 0;
+    int count = 0;
+
+    for(int i = 0;i < s.size();i++)
+    {
+        if(s[i] == '|')
+        {
+            flag++;
+        }
+
+        if(s[i] == '*' && flag % 2 == 0)
+        {
+            count++;
+        }
+    }
+
+    std::cout << count;
+
+    return 0;
+}
