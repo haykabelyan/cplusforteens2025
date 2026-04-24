f76d9f5c37817cf2ddf30a5356a836a8e1ff9647
Eduard711 <eduard2011davtyan@gmail.com>
2026-04-24 18:26:30 +0400

    1935.py

---
+++ string-1935.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string text = "hello world";
+    std::string brokenLetters = "ad";
+
+    
+}
---
+++ string-1935.py
@@ -0,0 +1,16 @@
+text = input()
+brokenLetters = input()
+
+text_arr = text.split(" ") # ["hello", "world"]
+count = 0
+
+for word in text_arr:
+    is_valid = True
+    for letter in word:
+        if letter in brokenLetters:
+            is_valid = False
+            break
+    if is_valid:
+        count += 1
+        
+print(count)
