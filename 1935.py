40848dc80e4d9694ef07ad77cdb6a57a35c02833
lianna947 <liannanersisian947@gmail.com>
2026-04-24 18:31:55 +0400

    1935

---
+++ 1935.py
@@ -0,0 +1,14 @@
+text = input("Enter text: ")
+brokenLetters = input("Enter broken letters: ")
+
+text_arr = text.split(" ") 
+count = 0
+for word in text_arr:
+    is_valid = True
+    for letter in word:
+        if letter in brokenLetters:
+            is_valid = False
+            break
+    if is_valid:
+        count += 1
+print(count)
