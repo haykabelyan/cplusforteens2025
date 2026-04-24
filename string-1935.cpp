47e163a91d6585438b5e030891c92811b3ce8c5c
Eduard711 <eduard2011davtyan@gmail.com>
2026-04-24 18:50:12 +0400

    1935

--- string-1935.cpp
+++ string-1935.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 #include <string>
+using namespace std;
 
 int main(){
-    std::string text = "hello world";
-    std::string brokenLetters = "ad";
+    string text = "hello world";
+    string brokenLetters = "ad";
 
-    
+    vector<string> text_arr;
+    stringstream ss(text);
+    string word;
+    while(ss >> word){
+        text_arr.push_back(word);
+    }
+
+    int count = 0;
+
+    for(string word : text_arr){
+        bool is_valid = true;
+        for(char letter : word){
+            if(brokenLetters.find(letter) != string::npos){
+                is_valid = false;
+                break;
+            }
+        }
+
+        if(is_valid){
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;   
 }
