9f73bfd68457d3497c4cb6663b95341276d83661
Eduard711 <eduard2011davtyan@gmail.com>
2026-05-01 17:46:07 +0400

    3174

--- string-3174.cpp
+++ string-3174.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
 
 int main(){
-    
+    std::string s;
+    std::cin >> s;
+    std::string res = "";
+
+    for(char c : s){
+        if(isdigit(c)){
+            res.pop_back();
+        }
+
+        else if(isalpha(c)){
+            res.push_back(c);
+        }
+    }
+
+    std::cout << res;
+    return 0;
 }
