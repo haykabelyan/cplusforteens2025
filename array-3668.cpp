1c0ab8aaab4d355f0ac6e78147374e5979580f16
Eduard711 <eduard2011davtyan@gmail.com>
2026-05-05 18:21:36 +0400

    3668

---
+++ array-3668.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> order = {3,1,2,5,4};
+    std::vector<int> friends = {1,3,4};
+    std::vector<int> all;
+
+    for(int i = 0; i < order.size(); i++){
+        for(int j = 0; j < friends.size(); j++){
+            if(order[i] == friends[j]){
+                all.push_back(order[i]);
+                break;
+            }
+        }
+    }
+
+    for(int i = 0; i<all.size(); i++){
+        std::cout << all[i];
+    }
+};
