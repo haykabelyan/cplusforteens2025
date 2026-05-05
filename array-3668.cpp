c2de7f5e4396071b9136c94074df9e36c16c19f0
nymbs <karen.vardanyan2009@gmail.com>
2026-05-05 18:07:53 +0400

    array-3668.cpp

--- array-3668.cpp
+++ array-3668.cpp
@@ -4,15 +4,18 @@
 int main(){
     std::vector<int> order = {3,1,2,5,4};
     std::vector<int> friends = {1,3,4};
-    std::vector<int> all = {};
+    std::vector<int> all;
 
-    for(int i = 0; i<order.size(); i++){
-        if(friends[i] == order[i]){
-            all.push_back(order[i]);
+        for(int i = 0; i < order.size(); i++){
+            for(int j = 0; j < friends.size(); j++){
+                if(order[i] == friends[j]){
+                    all.push_back(order[i]);
+                    break;
+                }
+            }
         }
-    }
 
-    for(int i = 0; i<all.size(); i++){
-        std::cout << all[i] << std::endl;
-    }
+        for(int i = 0; i<all.size(); i++){
+            std::cout << all[i] << std::endl;
+        }
 }
