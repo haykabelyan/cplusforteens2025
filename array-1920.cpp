4e7cdd75dcfb6a2af6813a30c575b0ae32dc38b1
Eduard711 <eduard2011davtyan@gmail.com>
2026-05-05 18:29:16 +0400

    1920.cpp

---
+++ array-1920.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> nums = {0,2,1,5,3,4};
+    std::vector<int> res;
+
+    for(int i = 0; i < nums.size(); i++){
+        res.push_back(nums[nums[i]]);
+    }
+    
+    for(int el : nums){
+        std::cout << el;
+    }
+}
