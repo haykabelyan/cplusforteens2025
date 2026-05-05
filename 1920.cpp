56a70a7cd1d8b653be337b8b4d7e58e06829bedb
lianna947 <liannanersisian947@gmail.com>
2026-05-05 18:26:23 +0400

    1920

---
+++ 1920.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    vector<int> ans(n);
+
+    for (int i = 0; i < n; i++) {
+        ans[i] = nums[nums[i]];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << " ";
+    }
+}
---
+++ 1920.py
@@ -0,0 +1,7 @@
+nums = list(map(int, input().split()))
+ans = []
+
+for el in nums:
+    ans.append(nums[el])
+
+print(ans)
