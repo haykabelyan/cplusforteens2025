4f507dd0f3651829107f1a624baa7351baa1590f
Vika741888 <vikaa.andriasyan741@gmail.com>
2026-05-05 18:07:54 +0400

    35668

--- array3668.cpp
+++ array3668.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main() {
 
@@ -12,9 +13,13 @@ int main() {
     for(int i=0;i<m;i++) {
         cin>>friends[i];
     }
-    
-
-
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+          if(order[i]==friends[j]) 
+          cout<<order[i];
+        }
+    }
 
     return 0;
+
 }
