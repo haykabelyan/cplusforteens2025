f8a366ff48de59c6a855fa62b4b7b6ee43dfe5d1
Vika741888 <vikaa.andriasyan741@gmail.com>
2026-05-08 18:18:13 +0400

    3701

--- array3701.cpp
+++ array3701.cpp
@@ -3,24 +3,19 @@
 using namespace std;
 int main() {
 
-    int n;
+    int n,s=0;
     cin>>n;
-    
     vector<int>nums(n);
-    
     for(int i=0;i<n;i++) {
         cin>>nums[i];
     }
+    for(int i=1;i<n;i+=2) {
+        nums[i]=-nums[i];
+    }
     for(int i=0;i<n;i++) {
-            if(i%2==0) {
-                nums[i]=nums[i];
-            } else nums[i]=-nums[i];
-        }
-        int s=0;
-        for(int i=0;i<n;i++) {
-          s+=nums[i];
-        }
-        cout<<s;
+        s+=nums[i];
+    }
+    cout<<s;
     
 
     return 0;
