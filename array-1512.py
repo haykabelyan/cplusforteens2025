023aebfa70429e0575c13a1d1205fe42ccff6a28
nymbs <karen.vardanyan2009@gmail.com>
2026-05-08 18:50:12 +0400

    array-3467.cpp

--- array-1512.py
+++ array-1512.py
@@ -3,7 +3,7 @@ count = 0
 
 for i in range(len(nums)):
     for j in range(i+1, len(nums)):
-            if (i == j):
+            if (nums[i] == nums[j]):
                 count += 1
     
 print(count)
---
+++ array-3467.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+    std::vector<int> nums = {4,3,2,1};
+    std::vector<int> ans;
+
+    for(int i = 0; i<nums.size(); i++){
+        if(nums[i] % 2 == 0){
+            ans.push_back(0);
+        }
+        else ans.push_back(1);
+    }
+    std::sort(ans.begin(), ans.end());
+
+    return ans;
+}
