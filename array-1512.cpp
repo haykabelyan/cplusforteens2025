6996a9111b535fdb4897989c33b915c1fca67ed7
nymbs <karen.vardanyan2009@gmail.com>
2026-05-08 18:36:49 +0400

    array-1512.cpp

--- array-1512.cpp
+++ array-1512.cpp
@@ -6,7 +6,7 @@ int main(){
     int count = 0;
 
     for(int i = 0; i<nums.size(); i++){
-        for(int j = 0; j<i; j++){
+        for(int j = i+1; j<nums.size(); j++){
             if(nums[i] == nums[j]){
                 count++;
             }
