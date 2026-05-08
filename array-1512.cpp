5d39de04f13e77372121bc348c51acb78d03fb3b
nymbs <karen.vardanyan2009@gmail.com>
2026-05-08 18:32:57 +0400

    array-1512.cpp

---
+++ array-1512.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> nums = {1,2,3,1,1,3};
+    int count = 0;
+
+    for(int i = 0; i<nums.size(); i++){
+        for(int j = 0; j<i; j++){
+            if(nums[i] == nums[j]){
+                count++;
+            }
+        }
+    }
+
+    std::cout << count;
+}
