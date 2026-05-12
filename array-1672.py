6258437b532a99d9cddc3a17c636f2c319be2ca7
GXAfam <khachatryan651@gmail.com>
2026-05-12 18:33:03 +0400

    1672.py

--- array-1672.py
+++ array-1672.py
@@ -1,10 +1,6 @@
 accounts = [[1,2,3],[3,2,1]]
-max = 0
+ans = []
 
 for tiv in accounts:
-    sum = 0
-    for i in range( len(tiv) ):
-        sum += tiv[i]
-    if max < sum:
-        max = sum
-print(max)
+    ans.append(sum(tiv))
+print(max(ans))
