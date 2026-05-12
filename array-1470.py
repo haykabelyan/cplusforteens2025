b9664277181a986d0b231cc3217d6a0d6b89cfce
nymbs <karen.vardanyan2009@gmail.com>
2026-05-12 18:04:51 +0400

    array-1470.py

--- array-1470.py
+++ array-1470.py
@@ -2,8 +2,8 @@ nums = [2,5,1,3,4,7]
 n = 3
 ans = []
 
-for i in nums:
-    ans.append(i)
-    ans.append(i + n)
+for i in range(n):
+    ans.append(nums[i])
+    ans.append(nums[i + n])
 
 print(ans)
