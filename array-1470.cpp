ec79febbe6f3caa21dc7fef809f725d000ba031d
nymbs <karen.vardanyan2009@gmail.com>
2026-05-12 17:52:59 +0400

    array-1470.cpp

---
+++ array-1470.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> nums = {2,5,1,3,4,7};
+    std::vector<int> ans = {};
+    int n = 3;
+
+    for(int i = 0; i<n; i++) {
+        ans.push_back(nums[i]);
+        ans.push_back(nums[i + n]);
+    }
+
+    for(int i = 0; i<ans.size(); i++){
+        std::cout << ans[i];
+    }
+
+}
