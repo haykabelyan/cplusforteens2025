5134be7c0b66c5884744613dc4bf576e6182bd8f
Eduard711 <eduard2011davtyan@gmail.com>
2026-05-15 20:37:55 +0400

    2798

---
+++ array-2798.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+
+int main(){
+    std::vector<int> hours = {0, 1, 2, 3, 4};
+
+    int target = 2;
+    int cnt = 0;
+
+    for(int i = 0; i < hours.size(); i++){
+        if(hours[i] >= target){
+            cnt++;
+        }
+    }
+
+    std::cout << cnt;
+
+    return 0;
+}
