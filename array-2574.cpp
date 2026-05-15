bca725d97b8f44a263c32984d168624e9eef4e02
Eduard711 <eduard2011davtyan@gmail.com>
2026-05-15 20:03:34 +0400

    2574

---
+++ array-2574.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+
+    int n;
+    std::cin >> n;
+    std::vector<int> nums(n);
+    for(int i=0 ;i < n; i++){
+        std::cin >> nums[i];
+    }
+
+    std::vector<int> leftsum(n);
+    std::vector<int> rightsum(n);
+    std::vector<int> result(n);
+
+    int s = 0;
+    for(int i = 0;i < n; i++){
+        for(int j = 0;j < i; j++){
+            s += nums[j];
+        }
+        leftsum[i] = s;
+    }
+    
+    int s = 0;
+    for(int i = 0;i<n;i++){
+        for(int j = i + 1; j < n; j++){
+            s += nums[j];
+        }
+        rightsum[i] = s;
+    }
+        
+    for(int i = 0; i < n; i++){
+        result[i] = abs(leftsum[i] - rightsum[i]);
+    }
+
+    for(int c : result) {
+        std::cout << c;
+    }
+
+    return 0;
+}
