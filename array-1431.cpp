4c5ef4044a7ab6095172de58d3691c60f8007970
nymbs <karen.vardanyan2009@gmail.com>
2026-05-15 20:28:15 +0400

    array-1431.cpp

---
+++ array-1431.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> candies = {2,3,5,1,3};
+    std::vector<bool> res;
+
+        int maxCandies = 0;
+        for (int i = 0; i<candies.size(); i++){
+            if (candies[i] > maxCandies) {
+                maxCandies = candies[i];
+            }
+        }
+
+        for(int i = 0; i<candies.size(); i++){
+            if(candies[i] + extraCandies >= maxCandies){
+                res.push_back(true);
+            }
+            else res.push_back(false);
+        }
+
+        for(int i = 0; i<res.size(); i++){
+            std::cout << res[i]; 
+        }
+}
