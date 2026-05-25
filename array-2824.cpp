199e9259d22e431203f3d98499f0110b76d0feff
GXAfam <khachatryan651@gmail.com>
2026-05-25 19:57:52 +0400

    2824

---
+++ array-2824.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> nums = {-1,1,2,3,1};
+    int target = 2;
+    int count = 0;
+
+    for(int i = 0; i < nums.size();i++)
+    {
+        for(int j = i + 1; j < nums.size(); j++)
+        {
+            if(nums[i] + nums[j] < target)
+            {
+                count++;
+            }
+        }
+    }
+    std::cout << count;
+    return 0;
+}
