d47e01cfc8e2d042514fa5796249a72f1df159e5
nymbs <karen.vardanyan2009@gmail.com>
2026-05-25 19:56:09 +0400

    array-2824.py

---
+++ array-2824.py
@@ -0,0 +1,10 @@
+nums = [-1,1,2,3,1]
+target = 2
+count = 0
+
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] < target:
+            count += 1
+
+print(count)
