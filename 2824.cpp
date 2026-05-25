6b6b0402f93210bdfd5af8eeb9ffac241f738264
lianna947 <liannanersisian947@gmail.com>
2026-05-25 19:57:07 +0400

    2824

---
+++ 2824.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    int target;
+    cin >> target;
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (arr[i]+ arr[j] < target) {
+                count++;
+            }
+        }
+    }
+
+    cout << count;
+}
