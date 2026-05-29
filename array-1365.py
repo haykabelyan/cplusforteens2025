a4ce8beb9a02ae2f16d79ce6155837377ff94aae
nymbs <karen.vardanyan2009@gmail.com>
2026-05-29 19:38:11 +0400

    array-1365.py

---
+++ array-1365.py
@@ -0,0 +1,14 @@
+nums = [8,1,2,2,3]
+count = []
+
+for i in range(len(nums)):
+            count1 = 0
+
+            for j in range(len(nums)):
+                if nums[j] < nums[i]:
+                    count1 += 1
+
+            count.append(count1)
+
+        return count
+
