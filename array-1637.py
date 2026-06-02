2be7ee664b3f8ca9e895aeb6d3b162e4cf0955c0
nymbs <karen.vardanyan2009@gmail.com>
2026-06-02 15:12:07 +0400

    array-1637.py

---
+++ array-1637.py
@@ -0,0 +1,14 @@
+points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
+x = []
+
+for point in points:
+   x.append(point[0])
+
+x.sort()
+
+mx = 0
+for i in range(1, len(x)):
+    mx = max(mx,  x[i] - x[i-1])
+
+
+print(mx)
