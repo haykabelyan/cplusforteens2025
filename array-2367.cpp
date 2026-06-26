3208011a1083a7d4ad038a52b01755dab0af3881
Eduard711 <eduard2011davtyan@gmail.com>
2026-06-26 18:00:19 +0400

    2367

---
+++ array-2367.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    vector<int> nums = {0, 1, 4, 6, 7, 10};
+    int diff = 3;
+    
+    int cnt = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+        for(int j = i + 1; j < nums.size(); j++){
+            for(int k = j + 1; k < nums.size(); k++){
+                if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) {
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    std::cout << cnt;
+
+    return 0
+}
