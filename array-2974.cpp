e642ca0a70bbfb639fe7e1c646b07c2c34b1c33d
Eduard711 <eduard2011davtyan@gmail.com>
2026-06-26 17:49:32 +0400

    2974

--- array-2974.cpp
+++ array-2974.cpp
@@ -2,5 +2,20 @@
 #include <vector>
 
 int main(){
-    
+
+    std::vector<int> nums = {5, 4, 2, 3};
+    std::vector<int> arr;
+
+    sort(nums.begin(),nums.end());
+
+    for(int i = 1; i < nums.size(); i+=2){
+        arr.push_back(nums[i]);
+        arr.push_back(nums[i - 1]);
+    }
+
+    for(int n : arr){
+        std::cout << n;
+    }
+
+    return 0;
 }
