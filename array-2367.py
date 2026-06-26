a06aefdde849cac3903581a0c0a2b1ce366835ab
Eduard711 <eduard2011davtyan@gmail.com>
2026-06-26 18:07:05 +0400

    2367.py

---
+++ array-2367.py
@@ -0,0 +1,11 @@
+nums = [0,1,4,6,7,10]
+diff = 3
+count = 0
+
+for i in range(0, len(nums)-2):
+    for j in range(i + 1, len(nums) - 1):
+        for z in range(j + 1, len(nums)):
+            if nums[j] - nums[i] == diff and nums[z] - nums[j] == diff:
+                count += 1
+
+print(count)
