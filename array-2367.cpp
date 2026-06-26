77501a0d10ccc2d2bb2eae0e2d03f7bd5ad0a801
Eduard711 <eduard2011davtyan@gmail.com>
2026-06-26 18:21:04 +0400

    2367

--- array-2367.cpp
+++ array-2367.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 int main() {
-    vector<int> nums = {0, 1, 4, 6, 7, 10};
+    std::vector<int> nums = {0, 1, 4, 6, 7, 10};
     int diff = 3;
     
     int cnt = 0;
@@ -10,7 +10,7 @@ int main() {
     for(int i = 0; i < nums.size(); i++){
         for(int j = i + 1; j < nums.size(); j++){
             for(int k = j + 1; k < nums.size(); k++){
-                if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) {
+                if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff){
                     cnt++;
                 }
             }
