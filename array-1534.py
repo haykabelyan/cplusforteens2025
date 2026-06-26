a2eb230ab22cceef8eaaa46085d389d27b64e893
Eduard711 <eduard2011davtyan@gmail.com>
2026-06-26 18:50:57 +0400

    1534.py

---
+++ array-1534.py
@@ -0,0 +1,13 @@
+arr = [3, 0, 1, 1, 9, 7]
+a = 7
+b = 2
+c = 3
+count = 0
+
+for i in range(0, len(arr) - 2):
+    for j in range(i + 1, len(arr) - 1):
+        for k in range( j+ 1, len(arr)):
+            if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                count += 1
+
+print(count)
