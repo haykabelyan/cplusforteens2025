c8b154b1a72bbbced283369177bdc0f64c7a9b05
Eduard711 <eduard2011davtyan@gmail.com>
2026-06-26 18:20:42 +0400

    1534

--- array-1534.cpp
+++ array-1534.cpp
@@ -11,9 +11,7 @@ int main() {
     for(int i = 0; i < arr.size(); i++) {
         for(int j = i + 1; j < arr.size(); j++){
             for(int k = j + 1; k < arr.size(); k++){
-                if(abs(arr[i] - arr[j]) <= a &&
-                    abs(arr[j] - arr[k]) <= b &&
-                    abs(arr[i] - arr[k]) <= c){
+                if(abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c){
                     cnt++;
                 }
             }
