375525b06ad62363e603ef7003efba5437928e9c
GXAfam <khachatryan651@gmail.com>
2026-06-26 17:40:45 +0400

    2974

---
+++ 2974.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> nums = {5, 4, 2, 3};
+    std::vector<int> arr;
+    sort(nums.begin(),nums.end());
+
+    for (int i = 1;i < nums.size();i+=2)
+    {
+        arr.push_back(nums[i]);
+        arr.push_back(nums[i-1]);
+    }
+
+    for(int el : arr)
+    {
+        std::cout << el;
+    }
+    return 0;
+}
