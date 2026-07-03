0c2b76bbc964b0865f5225073646262383e03ee5
Eduard711 <eduard2011davtyan@gmail.com>
2026-07-03 17:30:47 +0400

    2006

---
+++ array-2006.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> nums = {1, 2, 2, 1};
+    int k = 1;
+    int cnt = 0;
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(abs(nums[i] - nums[j]) == k){
+                cnt++;
+            }
+        }
+    }
+
+    std::cout << cnt;
+}
