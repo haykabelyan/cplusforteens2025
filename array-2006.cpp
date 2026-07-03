7cbd633e746d782735aef2b8ca65a02d79134a5b
Eduard711 <eduard2011davtyan@gmail.com>
2026-07-03 17:32:22 +0400

    2006

--- array-2006.cpp
+++ array-2006.cpp
@@ -6,7 +6,7 @@ int main(){
     int k = 1;
     int cnt = 0;
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < nums.size(); i++){
         for(int j = i + 1; j < n; j++){
             if(abs(nums[i] - nums[j]) == k){
                 cnt++;
