77a732813f6e0196a886b8a993ca51b162715a2e
Eduard711 <eduard2011davtyan@gmail.com>
2026-07-03 17:34:11 +0400

    2006.py

--- array-2006.py
+++ array-2006.py
@@ -1,4 +1,10 @@
-nums = {1, 2, 2, 1}
+nums = [1, 2, 2, 1]
 k = 1
 cnt = 0
 
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if abs(nums[i] - nums[j]) == k:
+            cnt += 1
+
+print(cnt)
